dffea50bef033f1ebb510b439c9665d0e4d9c3d8
raquelCSM <raquelsantos11@hotmail.com>
2025-04-03 18:32:07 -0300

    alterado

---
+++ GeradorNomeBanda.py
@@ -0,0 +1,4 @@
+print("Bem vindo ao gerador de nome de Banda")
+cidade = input("Qual cidade você nasceu?\n ")
+pet = input("Qual o nome do seu animal de estimação?\n ")
+print("O nome da sua banda pode ser "+ cidade + " "+ pet)
